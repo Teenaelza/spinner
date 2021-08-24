72293a48d2d9280f54ec13e5c16bb22d42781b93
Teenaelza <teena.thaliath@gmail.com>
2021-08-24 21:13:52 +0000

    Implemented cool spinner

---
+++ spinner1.js
@@ -0,0 +1,17 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+setTimeout(() => {
+  process.stdout.write('\r|💁💁💁💁💁💁💁  ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/😋😋😋😋😋😋😋   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-👌👌👌👌👌👌👌  ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\😍😍😍😍😍😍😍😍   '); 
+}, 700);
